141005bf10573ced177a82badd9e3d3b51644383
Nanci <sikabarca@gmail.com>
2020-05-15 20:33:14 -0500

    Add sika to developers

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -2,18 +2,21 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+PASSWORD = 'pi3.1415'
+DEVS = [
+    {'username': 'm', 'password': PASSWORD},
+    {'username': 'sika', 'password': PASSWORD}
+]
 
 
 class Command(BaseCommand):
     help = 'Seeds database'
 
     def handle(self, *args, **options):
-        try:
-            User.objects.create_superuser(
-                username='m',
-                password='pi3.1415'
-            )
-        except Exception:
-            raise CommandError('Database was not seed.')
+        for dev in DEVS:
+            try:
+                User.objects.create_superuser(**dev)
+            except Exception:
+                raise CommandError('Database was not seed.')
 
         self.stdout.write(self.style.SUCCESS('Database was seed successfully!'))
