8e709aba7e2c6c33691976e091944e72fd6f9d04
Nanci <sikabarca@gmail.com>
2020-05-18 17:48:17 -0500

    Setup jupyter

--- dentistry/settings.py
+++ dentistry/settings.py
@@ -161,3 +161,6 @@ AWS_S3_FILE_OVERWRITE = False
 # Media files
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Additional Notebook arguments to use
+NOTEBOOK_ARGUMENTS = ["--ip", "0.0.0.0", "--port", "8888", "--allow-root"]
