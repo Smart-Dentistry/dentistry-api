b79a2521a8f138f483cf94a0b37863f71cb971b6
M <m@mathsistor.com>
2020-05-18 22:49:45 -0500

    Use UserFactory instead of User.objects.create

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,18 +1,13 @@
-from django.contrib.auth import get_user_model
-
 import pytest
 
+from core.factories import UserFactory
 from .serializers import UserSerializer
-
-User = get_user_model()
 
 
 @pytest.mark.django_db
 def test_user_serializer_has_expected_fields():
     '''Test that UserSerializer has expected fields'''
-    user = User.objects.create_user(
-        username="user", password="password123", email="user@gmail.com"
-    )
+    user = UserFactory(username="user", password="password123", email="user@gmail.com")
     serializer = UserSerializer(user)
     data = serializer.data
     assert set(data.keys()) == {'id', 'username', 'email'}
