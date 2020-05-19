3801c4e0298eadaab3db44edf15d4c1d8fb6b935
Nanci <sikabarca@gmail.com>
2020-05-19 17:48:42 -0500

    Refactor seed

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -10,15 +10,20 @@ DEVS = [
 class Command(BaseCommand):
     help = 'Seeds database'
 
-    def handle(self, *args, **options):
+    def _create_devs(self):
         for dev in DEVS:
             try:
                 UserFactory(**dev, is_superuser=True, is_staff=True)
             except Exception:
                 raise CommandError('Database was not seed.')
 
+    def _create_patients(self):
         for _ in range(10):
             MalePatientFactory()
             FemalePatientFactory()
+
+    def handle(self, *args, **options):
+        self._create_devs()
+        self._create_patients()
 
         self.stdout.write(self.style.SUCCESS('Database was seed successfully!'))
