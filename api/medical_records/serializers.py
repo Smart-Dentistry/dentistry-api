4cd7b421e967b7a41c4635a4f2e79bd511b65d9c
M <m@mathsistor.com>
2020-05-18 19:14:48 -0500

    Implement PatientSerializer

---
+++ api/medical_records/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from medical_records.models import Patient
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    key = serializers.IntegerField(source="id", read_only=True)
+
+    class Meta:
+        model = Patient
+        fields = [
+            "key",
+            "photo",
+            "first_name",
+            "middle_name",
+            "last_name",
+            "second_last_name",
+            "id_document_number",
+            "sex",
+            "job_title",
+            "marital_status",
+            "birthdate",
+            "country_of_residence",
+            "address",
+            "phone",
+            "whatsapp",
+            "health_insurance_company",
+            "email",
+            "receive_promos",
+            "referral_source",
+            "emergency_contact",
+            "representative",
+            "family_history",
+            "personal_history",
+            "general_practitioners",
+        ]
