ed2facef6e98824d20ae29a8f4c90a274e9ef810
Nanci <sikabarca@gmail.com>
2020-05-19 17:42:00 -0500

    Create patients when seeding database

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from core.factories import UserFactory
+from core.factories import UserFactory, MalePatientFactory, FemalePatientFactory
 
 DEVS = [
     {'username': 'm', 'email': 'm@mathsistor.com'},
@@ -16,5 +16,9 @@ class Command(BaseCommand):
                 UserFactory(**dev, is_superuser=True, is_staff=True)
             except Exception:
                 raise CommandError('Database was not seed.')
+
+        for _ in range(10):
+            MalePatientFactory()
+            FemalePatientFactory()
 
         self.stdout.write(self.style.SUCCESS('Database was seed successfully!'))
