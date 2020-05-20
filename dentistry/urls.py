c40ff9b5dcf595719aa94967846d80e23a2dd947
Nanci <sikabarca@gmail.com>
2020-05-20 13:25:46 -0500

    Reorder list urlpatterns

--- dentistry/urls.py
+++ dentistry/urls.py
@@ -37,9 +37,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(api_urls)),
     re_path(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-    path("admin/", admin.site.urls),
-    path("api/", include(api_urls)),
 ]
