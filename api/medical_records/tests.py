95740e5f3c615d7f454bd4efd6f00c9b753f4940
Nanci <sikabarca@gmail.com>
2020-05-20 14:26:57 -0500

    Add test for PatientTableSerializer

--- api/medical_records/tests.py
+++ api/medical_records/tests.py
@@ -2,7 +2,7 @@ from medical_records.models import Patient
 
 import pytest
 
-from .serializers import PatientSerializer
+from .serializers import PatientSerializer, PatientTableSerializer
 
 
 @pytest.mark.django_db
@@ -54,4 +54,41 @@ def test_patient_serializer_has_expected_fields():
         "family_history",
         "personal_history",
         "general_practitioners",
+    }
+
+
+@pytest.mark.django_db
+def test_patient_table_serializer_has_expected_fields():
+    """Test that PatientTableSerializer has expected fields"""
+    patient = Patient.objects.create(
+        first_name="Mary",
+        middle_name="Jane",
+        last_name="Jones",
+        second_last_name="",
+        id_document_number="123456789",
+        sex="F",
+        job_title="Engineer",
+        marital_status="SI",
+        birthdate="2010-01-01",
+        country_of_residence="A",
+        address=None,
+        phone="+593987878787",
+        whatsapp=False,
+        health_insurance_company="",
+        email="mary@example.com",
+        receive_promos=True,
+        referral_source="S",
+    )
+    serializer = PatientTableSerializer(patient)
+    data = serializer.data
+    assert set(data.keys()) == {
+        "key",
+        "photo",
+        "first_name",
+        "last_name",
+        "id_document_number",
+        "birthdate",
+        "phone",
+        "whatsapp",
+        "email",
     }
