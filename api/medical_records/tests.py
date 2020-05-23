dba47d0e51d733d1523a1d0f06bf8e300f5f4bfe
Nanci <sikabarca@gmail.com>
2020-05-22 19:02:04 -0500

    Use factory in tests

--- api/medical_records/tests.py
+++ api/medical_records/tests.py
@@ -1,33 +1,14 @@
-from medical_records.models import Patient
-
 from datetime import datetime
 import pytest
 
 from .serializers import PatientSerializer, PatientTableSerializer
+from core.factories import MalePatientFactory
 
 
 @pytest.mark.django_db
 def test_patient_serializer_has_expected_fields():
     """Test that PatientSerializer has expected fields"""
-    patient = Patient.objects.create(
-        first_name="Mary",
-        middle_name="Jane",
-        last_name="Jones",
-        second_last_name="",
-        id_document_number="123456789",
-        sex="F",
-        job_title="Engineer",
-        marital_status="SI",
-        birthdate="2010-01-01",
-        country_of_residence="A",
-        address=None,
-        phone="+593987878787",
-        whatsapp=False,
-        health_insurance_company="",
-        email="mary@example.com",
-        receive_promos=True,
-        referral_source="S",
-    )
+    patient = MalePatientFactory()
     serializer = PatientSerializer(patient)
     data = serializer.data
     assert set(data.keys()) == {
@@ -61,25 +42,9 @@ def test_patient_serializer_has_expected_fields():
 @pytest.mark.django_db
 def test_patient_table_serializer_has_expected_fields():
     """Test that PatientTableSerializer has expected fields"""
-    patient = Patient.objects.create(
-        first_name="Mary",
-        middle_name="Jane",
-        last_name="Jones",
-        second_last_name="",
-        id_document_number="123456789",
-        sex="F",
-        job_title="Engineer",
-        marital_status="SI",
-        birthdate=datetime.strptime("2010-01-01", "%Y-%m-%d").date(),
-        country_of_residence="A",
-        address=None,
-        phone="+593987878787",
-        whatsapp=False,
-        health_insurance_company="",
-        email="mary@example.com",
-        receive_promos=True,
-        referral_source="S",
-    )
+    patient = MalePatientFactory(
+        birthdate=datetime.strptime("2010-01-01", "%Y-%m-%d").date()
+        )
     serializer = PatientTableSerializer(patient)
     data = serializer.data
     assert set(data.keys()) == {
