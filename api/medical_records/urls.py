d0882afd286cc420c3a6a96c7b0ed8e9e6935f89
M <m@mathsistor.com>
2020-05-28 23:44:29 -0500

    Implement service to retrieve provinces of Ecuador

--- api/medical_records/urls.py
+++ api/medical_records/urls.py
@@ -1,8 +1,12 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from .views import PatientViewSet
+from .views import PatientViewSet, provinces_of_ecuador
 
 router = routers.SimpleRouter()
 router.register(r'patients', PatientViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('provinces-of-ecuador/', provinces_of_ecuador, name='provinces_of_ecuador')
+]
--- api/medical_records/views.py
+++ api/medical_records/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import PatientTableSerializer, PatientSerializer
 from medical_records.models import Patient
+from medical_records.constants import PROVINCES_OF_ECUADOR
 
 
 class PatientViewSet(viewsets.ModelViewSet):
@@ -15,3 +17,8 @@ class PatientViewSet(viewsets.ModelViewSet):
         queryset = Patient.objects.all()
         serializer = PatientTableSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+@api_view()
+def provinces_of_ecuador(request):
+    return Response(PROVINCES_OF_ECUADOR)
