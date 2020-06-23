f6a2c153ec76beebd3ce16d7def20b8877e25763
M <m@mathsistor.com>
2020-06-22 22:34:41 -0500

    Set up sending email with sendgrid

--- dentistry/settings.py
+++ dentistry/settings.py
@@ -187,3 +187,12 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
 }
+
+# SendGrid
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'm@mathsistor.com'
