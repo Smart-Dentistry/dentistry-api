4ba10c49c26980cde878282b2c60a621f7a7dfaa
Nanci <sikabarca@gmail.com>
2020-08-01 00:06:22 -0500

    Register model in admin

--- medical_records/admin.py
+++ medical_records/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import MedicalBackground, Patient
+from .models import MedicalBackground, Patient, PeriodontalExam
 
 admin.site.register(Patient)
 admin.site.register(MedicalBackground)
+admin.site.register(PeriodontalExam)
